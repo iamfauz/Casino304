051f441403e1930ff26bea0fdb3c58d81fbed386
Fawaz Ahmed <iamfauz@yahoo.com>
2020-03-30 00:27:12 -0700

    Created a skeleton Node.js app with possiple dependencies

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 8000;
+
+// when a random route is inputed
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to this API.'
+}));
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+});
+export default app;
