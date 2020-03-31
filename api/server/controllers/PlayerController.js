711a2770824af4011a3ead3c88965ec6dd4c32dc
Fawaz Ahmed <iamfauz@yahoo.com>
2020-03-30 23:56:39 -0700

    Added endpoint for login by player

--- api/server/controllers/PlayerController.js
+++ api/server/controllers/PlayerController.js
@@ -16,6 +16,29 @@ class PlayerController {
                 res.json(players)
             })
     }
+
+    // Login for players 
+      static async loginByPlayer(req, res) {
+        const email = req.body.email;
+        console.log("email: "  + email);
+        const password = req.body.password;
+        console.log("password: "  + password);
+        const query = 'SELECT * FROM players WHERE email = :email AND password = :password;'
+        connection.query(query, { type: connection.QueryTypes.SELECT,
+            replacements: {
+                email: email,
+                password: password
+              } })
+            .then(players => {
+                console.log([players])
+                if(players.length != 0){
+                    res.json(players[0])
+                } else {
+                    res.status(404).json({message: "Email or Password wrong"})
+                }
+            })
+    }
+    
 }
 
 export default PlayerController;
--- api/server/routes/playerRoutes.js
+++ api/server/routes/playerRoutes.js
@@ -3,7 +3,14 @@ import PlayerController from '../controllers/PlayerController';
 
 const router = Router();
 
-// Define all player related endpoints/routes i.e all endpoints starting with "http://localhost:3000/players"
+/**
+ * Define all player related endpoints/routes i.e all endpoints starting with "http://localhost:3000/players"
+ * */
+
+
 router.get('/', PlayerController.getAllPlayers);
+
+// Body of request should include email and password
+router.get('/login', PlayerController.loginByPlayer);
 
 export default router;
